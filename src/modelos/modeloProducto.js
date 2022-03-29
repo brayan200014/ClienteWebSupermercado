b430d398993b90d7a392329c36b015cc0a1c89e9
IkerOrt2001 <ikerortezcolindres@gmail.com>
2022-03-28 21:31:27 -0600

    Modelo y controlador Productos

---
+++ src/modelos/modeloProducto.js
@@ -0,0 +1,43 @@
+const sequelize  =  require('sequelize');
+const db = require('../configuraciones/db');
+const Categorias= require('../modelos/modeloCategorias')
+
+const Producto = db.define("producto",
+{
+    IdProducto:{
+        type: sequelize.INTEGER,
+        primaryKey: true ,
+        autoIncrement: true,
+        allowNull: false,      
+    },
+    NombreProducto:{
+        type: sequelize.STRING(45),
+        allowNull: false,
+    },
+    DescripcionProducto:{
+        type: sequelize.STRING(100),
+        allowNull: true,
+    },
+   ISV:{
+        type: sequelize.DOUBLE,
+        allowNull: true,
+    },
+    Estado:{
+        type: sequelize.TINYINT(1),
+        allowNull: true,
+       
+    },
+    Imagen:{
+        type: sequelize.STRING(250),
+        allowNull: true,
+    }
+},
+{
+    tableName: "productos",
+    timestamps : false,
+}
+
+);
+
+Categorias.hasOne(Producto, {foreignKey: 'Categorias_IdCategoria'});
+module.exports=Producto;
